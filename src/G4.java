67f58ce3675114f5c721a9c0a540598fd589f9ea
chenfm-wa15 <33310612+chenfm-wa15@users.noreply.github.com>
2017-11-02 15:54:11 +0800

    Update G4.java

--- src/G4.java
+++ src/G4.java
@@ -12,5 +12,6 @@ public class G4 {
     public static void main(String[] args){
         System.out.println("Main");
         System.out.println("First branch");
+        System.out.println("test");
     }
 }

ab2c45c527132044314e4510c3b6c69da387c2e5
Student <Student@PC97>
2017-11-09 15:40:36 +0800

    Merge origin/master
    Conflicts:
    	build.xml
    	nbproject/build-impl.xml
    	nbproject/genfiles.properties
    	nbproject/project.properties
    	nbproject/project.xml

--- src/javaapplication1/JavaApplication1.java
+++ src/javaapplication1/JavaApplication1.java
@@ -3,7 +3,7 @@ package javaapplication1;
 public class JavaApplication1 {
 
     public static void main(String[] args) {
-        System.out.println("1");
+        System.out.println("12");
     }
     
 }

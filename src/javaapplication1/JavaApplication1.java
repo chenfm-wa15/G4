d7b6ab8ba60d270678315b6b5e3bb62554f071d8
Student <Student@PC97>
2017-11-09 15:30:49 +0800

    [Finish #152708415]

--- src/javaapplication1/JavaApplication1.java
+++ src/javaapplication1/JavaApplication1.java
@@ -3,7 +3,7 @@ package javaapplication1;
 public class JavaApplication1 {
 
     public static void main(String[] args) {
-        System.out.println("test");
+        System.out.println("1");
     }
     
 }
